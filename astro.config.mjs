38b3bd7361d438123d16101d353ec6301187b96c
nbaulib <69161557+nbaulib@users.noreply.github.com>
2026-04-30 10:48:31 -0400

    deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
+  site: 'https://nbaulib.github.io',
   integrations: [
     mdx({
       remarkPlugins: [],
