2bbe3fddf9497f66e7d26077005d1eaf07e0ed74
nbaulib <69161557+nbaulib@users.noreply.github.com>
2026-04-30 11:38:51 -0400

    update astro + fix thumbnail image

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,28 +1,19 @@
-// src/content.config.ts
-
-// 1. Import utilities from `astro:content`
 import { defineCollection } from 'astro:content';
-
-// 2. Import loader
 import { glob } from 'astro/loaders';
-
-// 3. Import Zod
 import { z } from 'astro/zod';
 
-// 4. Define a `loader` and `schema` for each collection
 const projects = defineCollection({
   loader: glob({ base: './src/content/projects', pattern: '**/*.mdx' }),
-  schema: z.object({
+  schema: ({ image }) => z.object({
     title: z.string(),
     order: z.number().optional(),
     timeline: z.string(),
     role: z.string(),
     collaborators: z.array(z.string()),
     tools: z.array(z.string()),
-    thumbnail: z.string().optional(),
+    thumbnail: image(),
     desc: z.string(),
   }),
 });
 
-// 5. Export a single `collections` object to register your collection(s)
 export const collections = { projects };
